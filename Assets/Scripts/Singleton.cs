199ae18abbc332cccc17ee41da650c818238e87c
Yoav Saroya <stankovic100@gmail.com>
2017-08-24 17:31:37 +0300

    Adding view and start Coding

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+//T must inherits from MonoBehaviour
+public class Singleton<T>: MonoBehaviour where T : MonoBehaviour {
+
+    private static T instance;
+
+    public static T Instance {
+        get {
+            T memorizedObject = FindObjectOfType<T>();
+            if(instance == null)
+                instance = memorizedObject;
+            else if(instance != memorizedObject)
+                Destroy(memorizedObject);
+            return instance;
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : Singleton<ZombieManager> {
+
+    private SpriteRenderer spriteRenderer;
+    private List<Zombie> zombieList = new List<Zombie>();
+    private List<Collider2D> buildList = new List<Collider2D>();
+    private Collider2D buildTile;
+
+    private void Start () {
+		
+	}
+	
+	private void Update () {
+		
+	}
+}
